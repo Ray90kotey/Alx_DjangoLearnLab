86e461a9114ffca4524fb65ca3f1defde3b4f9eb
Ray90kotey <Raykotey5@gmail.com>
2026-01-24 16:27:13 +0000

    ww

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
@@ -13,3 +16,25 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+class UserLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+
+class UserLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
+
+class UserLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "relationship_app/register.html", {"form": form})
