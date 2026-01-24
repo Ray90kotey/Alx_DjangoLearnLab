95d32cd9c12e138ff0f94dff4db91531daa4319d
Ray90kotey <Raykotey5@gmail.com>
2026-01-24 12:48:10 +0000

    mod

---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,26 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+
+# Query all books by a specific author
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return author.books.all()
+
+
+# List all books in a library
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+
+# Retrieve the librarian for a library
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.librarian
+
+
+# Sample usage (for testing in Django shell)
+if __name__ == "__main__":
+    print(books_by_author("Chinua Achebe"))
+    print(books_in_library("Central Library"))
+    print(librarian_for_library("Central Library"))
